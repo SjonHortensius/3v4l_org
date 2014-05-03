fd0d1cca49b06d04c4a68d6608147339246af67b
Sjon Hortensius <sjon@hortensius.net>
2014-05-03 20:06:18 +0200

    some hhvm styling plus alias fixes
    convert-aliases - updated after testing
    my.css - always show full dd when there is only 1
    get_hhvm - include a notification

--- convert-aliases.sh
+++ convert-aliases.sh
@@ -3,36 +3,41 @@ set -e
 cd `dirname $0`
 
 updateScript() {
-    cat /var/lxc/php_shell/in/$1 | php -r 'print trim(str_replace(array("\r\n", "\r"), "\n", file_get_contents("php://stdin")));' >/var/lxc/php_shell/in/$2
+	php -r 'file_put_contents($argv[2], trim(str_replace(array("\r\n", "\r"), "\n", file_get_contents($argv[1]))));' /var/lxc/php_shell/in/$1 /var/lxc/php_shell/in/$2
 }
 
 fileHash() {
-    php -r 'echo gmp_strval(gmp_init(sha1(trim(str_replace(array("\r\n", "\r"), "\n", file_get_contents("/var/lxc/php_shell/in/". $argv[1])))), 16), 58)."\n";' $*
+    php -r 'echo gmp_strval(gmp_init(sha1(trim(str_replace(array("\r\n", "\r"), "\n", file_get_contents($argv[1])))), 16), 58)."\n";' /var/lxc/php_shell/in/$1
 }
 
 updateDb() {
-    OLD=$1 NEW=$2
-    [[ ! -f /var/lxc/php_shell/in/$OLD ]] && exit 1
+	OLD=$1 NEW=$2
 
-    cd / ; sudo -u postgres psql -h 127.0.0.1 phpshell -tc "
-        UPDATE input SET alias = '$OLD' WHERE short = '$NEW'; \
-        UPDATE submit SET input = '$NEW' WHERE input = '$OLD' AND ip NOT IN(SELECT ip FROM submit WHERE input ='$NEW'); \
-        UPDATE submit s SET \"count\" = \"count\" +(SELECT \"count\" FROM submit WHERE ip = s.ip AND input = '$OLD') WHERE input = '$NEW'; \
-        UPDATE submit s SET created = COALESCE((SELECT created FROM submit WHERE ip = s.ip AND input = '$OLD'), created) WHERE input = '$NEW'; \
-        UPDATE submit s SET updated = COALESCE(updated, (SELECT updated FROM submit WHERE ip = s.ip AND input = '$OLD')) WHERE input = '$NEW'; \
-        DELETE FROM submit WHERE input = '$OLD'; \
-        UPDATE input SET source = '$NEW' WHERE source = '$OLD'; \
-        UPDATE input SET source = null WHERE short = '$NEW' AND source = '$OLD'; \
-        DELETE FROM input where short='$OLD'; \
-        " && mv -v /var/lxc/php_shell/in/$OLD /var/lxc/php_shell/in/ALIASED/
+#	UPDATE submit s SET \"count\" = \"count\" +(SELECT \"count\" FROM submit WHERE ip = s.ip AND input = '$OLD') WHERE input = '$NEW'; \
+#	UPDATE submit s SET created = COALESCE((SELECT created FROM submit WHERE ip = s.ip AND input = '$OLD'), created) WHERE input = '$NEW'; \
+#	UPDATE submit s SET updated = COALESCE(updated, (SELECT updated FROM submit WHERE ip = s.ip AND input = '$OLD')) WHERE input = '$NEW'; \
+
+	cd / ; sudo -u postgres psql -h 127.0.0.1 phpshell -tc "
+		UPDATE submit SET input = '$NEW' WHERE input = '$OLD' AND ip NOT IN(SELECT ip FROM submit WHERE input ='$NEW'); \
+		UPDATE input SET alias = '$OLD' WHERE short = '$NEW'; \
+		UPDATE input SET source = null WHERE short = '$NEW' AND source = '$OLD'; \
+		UPDATE input SET source = '$NEW' WHERE source = '$OLD'; \
+		UPDATE result SET input = '$NEW' WHERE input = '$OLD' AND run > COALESCE((SELECT max(run) FROM result WHERE input = '$NEW'), 0); \
+		DELETE FROM input WHERE short='$OLD' AND (SELECT COUNT(*) FROM submit WHERE input = '$OLD') = 0; \
+		SELECT input,ip FROM submit WHERE input = '$OLD'; \
+	"
+
+	mv -v /var/lxc/php_shell/in/$OLD /var/lxc/php_shell/in/ALIASED/
 }
 
 cat all-hashes.txt | while read f h
 do
-    [[ $h == *$f ]] && { echo UP_TO_DATE ; continue ; }
+	[[ ! -f /var/lxc/php_shell/in/$f ]] && { echo ERROR:$f ; exit 1; }
+	[[ $h == *$f ]] && { echo UP_TO_DATE ; continue ; }
 
-    l=${#h} ; n=${h:$l-5:5}
-    [[ ! -f /var/lxc/php_shell/in/$n ]] && { echo COPY $f $n ; updateScript $f $n ; }
+	l=${#h} ; n=${h:$l-5:5}
+	[[ ! -f /var/lxc/php_shell/in/$n ]] && { echo COPY $f $n ; updateScript $f $n ; }
 
-    echo ALIAS:$f:$n
+	echo ALIAS:$f:$n
+	updateDb $f $n
 done
--- tpl/get_hhvm.php
+++ tpl/get_hhvm.php
@@ -1,7 +1,18 @@
+<div class="alert">
+	<h2>Beta hhvm output</h2>
+	<p>It took a lot of effort to get hhvm working; please let me know what you think!<br/>Ideas are also welcome; my ToDo list currently includes:</p>
+	<ul>
+		<li>Add <i>hhvm</i> tab / merge with PHP output</li>
+		<li>Add <i>hhvm</i> timings to performance tab</li>
+		<li>Detect &lt;?hh prefix and skip php &amp; vld</li>
+	</ul>
+</div>
 <?php
 if (empty($this->data))
-	print('Something went wrong');
-else {
-?><pre><?=$this->data?></pre>
-<p>Generated using <a href="http://hhvm.com/">HHVM</a>, version 3.0.1</p>
+	print('No hhvm output found!');
+else
+{
+?><dl><dt id="v301">hhvm-3.0.1</dt><dd><?=$this->data?></dd></dl>
+<p>Generated using <a href="http://hhvm.com/">HHVM</a></p>
 <? } ?>
+
