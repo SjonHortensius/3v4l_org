1c5cba62a14aa05b9b08b8b19b5d5ea3822d2d8a
Sjon Hortensius <sjon@hortensius.net>
2015-04-19 21:35:53 +0200

    Show recent submissions on index

--- library/PhpShell/Action/Index.php
+++ library/PhpShell/Action/Index.php
@@ -3,4 +3,11 @@
 class PhpShell_Action_Index extends PhpShell_Action_New
 {
 	public $bodyClass = 'index';
+
+	public function init()
+	{
+		parent::init();
+
+		$this->last = new PhpShell_LastScriptsList(PhpShell_Input, null, [], ['id' => false]);
+	}
 }
