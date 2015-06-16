1ead81a570e3287af16ec7b536c51ec335b0e495
Sjon Hortensius <sjon@hortensius.net>
2015-06-16 21:58:06 +0200

    update ga script, move php.ini to tpl

--- library/PhpShell/Action/About.php
+++ library/PhpShell/Action/About.php
@@ -1,15 +1,3 @@
 <?php
-
-class PhpShell_Action_About extends PhpShell_Action
-{
-	public $phpIni;
-
-	public function run()
-	{
-		#avoid open_basedir @production
-		$base = PhpShell_Input::PATH;
-		$this->phpIni = `cat $base/../etc/php.ini`;
-
-		return parent::run();
-	}
-}
+// required so we don't interpret 'about' as a script
+class PhpShell_Action_About extends PhpShell_Action {}
