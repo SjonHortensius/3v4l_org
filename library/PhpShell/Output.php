d07fe7d7086bda3c782170ec68d35dc61310d494
Sjon Hortensius <sjon@hortensius.net>
2015-08-19 11:51:09 +0200

    Output - limit output to 32K, or we exceed memory_limit

--- library/PhpShell/Output.php
+++ library/PhpShell/Output.php
@@ -13,7 +13,8 @@ class PhpShell_Output extends PhpShell_Entity
 		if (!isset($this->_raw))
 			$this->_raw = ltrim(stream_get_contents($this->raw), "\n");
 
-		$raw = preg_replace('~(?<![\\\])\006~', $version, $this->_raw);
+		$raw = substr($this->_raw, 0, 32768);
+		$raw = preg_replace('~(?<![\\\])\006~', $version, $raw);
 		$raw = preg_replace('~(?<![\\\])\007~', $input->short, $raw);
 		return str_replace(array('\\'.chr(6), '\\'.chr(7)), array(chr(6), chr(7)), $raw);
 	}
