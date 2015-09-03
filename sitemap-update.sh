108223d5d89b59316a38f0b4ca09fb1310ec59c4
Sjon Hortensius <sjon@hortensius.net>
2015-09-03 12:59:27 +0200

    sitemap - https consistent, script - schema.org updates

--- sitemap-update.sh
+++ sitemap-update.sh
@@ -7,7 +7,7 @@ ls /srv/http/3v4l.org/in -l --time-style='+%Y-%m-%d' | while read x x x x x d n
 do
 	[[ -z $n ]] && continue
 
-	echo -e "<url><loc>https://3v4l.org/$n</loc><lastmod>$d</lastmod><changefreq>monthly</changefreq></url>"
+	echo "<url><loc>https://3v4l.org/$n</loc><lastmod>$d</lastmod><changefreq>monthly</changefreq></url>"
 done | split --verbose -dl 50000 - sitemap
 
 echo '<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' > sitemap.xml
@@ -17,7 +17,7 @@ do
 	echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' > $f.xml
 	cat $f >> $f.xml
 	echo '</urlset>' >> $f.xml
-	echo "<sitemap><loc>http://3v4l.org/$f.xml</loc><lastmod>`date +%Y-%m-%d`</lastmod></sitemap>" >> sitemap.xml
+	echo "<sitemap><loc>https://3v4l.org/$f.xml</loc><lastmod>`date +%Y-%m-%d`</lastmod></sitemap>" >> sitemap.xml
 	rm $f
 done
 
