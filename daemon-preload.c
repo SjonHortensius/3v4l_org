54538d53b10d70dfb948ffd5acde223d1201d040
Sjon Hortensius <sjon@hortensius.net>
2016-05-25 19:23:51 +0200

    preload - replace _init with if-statement per overload, fixes segfault with hhvm

--- daemon-preload.c
+++ daemon-preload.c
@@ -8,50 +8,18 @@
 
 int diff;
 
-struct tm *(*org_localtime)(const time_t *timep, struct tm *result);
+int (*org_gettimeofday)(struct timeval *tp, void *tzp);
 time_t (*org_time)(time_t *tloc);
-
-int gettimeofday(struct timeval *tv, struct timezone *tz) {
-	tv->tv_sec = org_time(NULL) - diff;
-
-	return 0;
-}
-
-int clock_gettime(clockid_t clk_id, struct timespec *tp) {
-	if (tp == NULL)
-		return -1;
-
-	tp->tv_sec = org_time(NULL) - diff;
-
-	return 0;
-}
-
-time_t time(time_t *t) {
-	int t_ = org_time(NULL) - diff;
-
-	if (t)
-		*t = t_;
-
-	return t_;
-}
-
-int ftime(struct timeb *tp) {
-	tp->time = org_time(NULL) - diff;
-
-	return 0;
-}
-
-struct tm *localtime_r(const time_t *timep, struct tm *result)
-{
-	time_t t = (time_t)(org_time(NULL) - diff);
-	return org_localtime(&t,result);
-}
+int (*org_clock_gettime)(clockid_t clk_id, struct timespec *tp);
+struct tm *(*org_localtime_r)(const time_t *timep, struct tm *result);
 
 void
-_init(void)
+_initLib(void)
 {
-	org_localtime = dlsym(RTLD_NEXT, "localtime_r");
-	org_time = dlsym(RTLD_NEXT, "time");
+	org_gettimeofday =  dlsym(RTLD_NEXT, "gettimeofday");
+	org_time =          dlsym(RTLD_NEXT, "time");
+	org_clock_gettime = dlsym(RTLD_NEXT, "clock_gettime");
+	org_localtime_r =   dlsym(RTLD_NEXT, "localtime_r");
 
 	int offset = 0;
 	if (getenv("TIME") != NULL) {
@@ -61,5 +29,60 @@ _init(void)
 	unsetenv("TIME");
 	unsetenv("LD_PRELOAD");
 
-	diff = time(NULL) - offset;
+	diff = org_time(NULL) - offset;
+}
+
+int gettimeofday(struct timeval *restrict tp, struct timezone *restrict tzp) {
+	if (0 == diff)
+		_initLib();
+
+	int r = org_gettimeofday(tp, tzp);
+
+	if (!r)
+		return r;
+
+	tp->tv_sec -= diff;
+	return 0;
+}
+
+time_t time(time_t *t) {
+	if (0 == diff)
+		_initLib();
+
+	time_t r = org_time(t) - diff;
+
+	if (t)
+		*t = r;
+
+	return r;
+}
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp) {
+	if (0 == diff)
+		_initLib();
+
+	int r = org_clock_gettime(clk_id, tp);
+
+	if (!r)
+		return r;
+
+	tp->tv_sec -= diff;
+	return 0;
+}
+
+struct tm *localtime_r(const time_t *timep, struct tm *result) {
+	if (0 == diff)
+		_initLib();
+
+	time_t t = (time_t)(org_time(NULL) - diff);
+	return org_localtime_r(&t, result);
+}
+
+int ftime(struct timeb *tp) {
+	if (0 == diff)
+		_initLib();
+
+	tp->time = org_time(NULL) - diff;
+
+	return 0;
 }
