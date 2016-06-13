eb5e132c268749618bde0bdf43ffb89af5354a52
Sjon Hortensius <sjon@hortensius.net>
2016-06-13 14:03:45 +0200

    preload - convert diff into a time_t and fixate milliseconds as well

--- daemon-preload.c
+++ daemon-preload.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <sys/timeb.h>
 
-int diff;
+struct timeval diff;
 
 int (*org_gettimeofday)(struct timeval *tp, void *tzp);
 time_t (*org_time)(time_t *tloc);
@@ -27,24 +27,31 @@ _initLib(void)
 	unsetenv("TIME");
 	unsetenv("LD_PRELOAD");
 
-	diff = org_time(NULL) - offset;
+	org_gettimeofday(&diff, NULL);
+	diff.tv_sec -= offset;
 }
 
 int gettimeofday(struct timeval *restrict tp, struct timezone *restrict tzp) {
-	if (0 == diff)
+	if (0 == diff.tv_sec)
 		_initLib();
 
 	org_gettimeofday(tp, tzp);
-	tp->tv_sec -= diff;
+
+	tp->tv_sec -= diff.tv_sec;
+	if (tp->tv_usec < diff.tv_usec) {
+		tp->tv_sec--;
+		tp->tv_usec = tp->tv_usec + 1000000 - diff.tv_usec;
+	} else
+		tp->tv_usec -= diff.tv_usec;
 
 	return 0;
 }
 
 time_t time(time_t *t) {
-	if (0 == diff)
+	if (0 == diff.tv_sec)
 		_initLib();
 
-	time_t r = org_time(t) - diff;
+	time_t r = org_time(t) - diff.tv_sec;
 
 	if (t)
 		*t = r;
@@ -53,32 +60,39 @@ time_t time(time_t *t) {
 }
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp) {
-	if (0 == diff)
+	if (0 == diff.tv_sec)
 		_initLib();
 
 	int r = org_clock_gettime(clk_id, tp);
 
 	if (0 == r)
-		tp->tv_sec -= diff;
+		tp->tv_sec -= diff.tv_sec;
 
 	return r;
 }
 
 struct tm *localtime_r(time_t *timep, struct tm *result) {
-	if (0 == diff)
+	if (0 == diff.tv_sec)
 		_initLib();
 
 	if (!timep)
-		*timep = (time_t)(org_time(NULL) - diff);
+		*timep = (time_t)(org_time(NULL) - diff.tv_sec);
 
 	return org_localtime_r(timep, result);
 }
 
 int ftime(struct timeb *tp) {
-	if (0 == diff)
+	if (0 == diff.tv_sec)
 		_initLib();
 
-	tp->time = org_time(NULL) - diff;
+	ftime(tp);
+
+	tp->time -= diff.tv_sec;
+	if (tp->millitm < diff.tv_usec) {
+		tp->millitm--;
+		tp->millitm = tp->millitm + 1000000 - diff.tv_usec;
+	} else
+		tp->millitm -= diff.tv_usec;
 
 	return 0;
 }
