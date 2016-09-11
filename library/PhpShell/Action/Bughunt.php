62450e7135f8b8fb28907ba292c14a9f6ee20e1a
Sjon Hortensius <sjon@hortensius.net>
2016-09-11 15:08:26 +0200

    Improve bughunt performance, don't DISTINCT and don't filter on exitCode

--- library/PhpShell/Action/Bughunt.php
+++ library/PhpShell/Action/Bughunt.php
@@ -55,7 +55,7 @@ class PhpShell_Action_Bughunt extends PhpShell_Action
 		foreach (Basic::$userinput['versions'] as $i => $v)
 		{
 			$alias = 'v'.$i;
-			$q .= "\nAND {$alias}.\"exitCode\" NOT IN(255, 137) AND {$alias}.version = ?".($i>0? " AND {$alias}.output = v0.output" : "");
+			$q .= "\nAND {$alias}.version = ?".($i>0? " AND {$alias}.output = v0.output" : "");
 
 			array_push($joins, ['result_current', "{$alias}.input = input.id AND {$alias}.run = input.run", $alias]);
 			array_push($params, PhpShell_Version::byName($v)->id);
@@ -64,12 +64,12 @@ class PhpShell_Action_Bughunt extends PhpShell_Action
 		foreach (Basic::$userinput['controls'] as $i => $v)
 		{
 			$alias = 'c'.$i;
-			$q .= "\nAND {$alias}.\"exitCode\" NOT IN(255, 137) AND {$alias}.version = ? AND {$alias}.output != v0.output".($i>0? " AND {$alias}.output = c0.output" : "");
+			$q .= "\nAND {$alias}.version = ? AND {$alias}.output != v0.output".($i>0? " AND {$alias}.output = c0.output" : "");
 			array_push($joins, ['result_current', "{$alias}.input = input.id AND {$alias}.run = input.run", $alias]);
 			array_push($params, PhpShell_Version::byName($v)->id);
 		}
 
-		$q .= "\nAND input.id NOT IN (SELECT DISTINCT input FROM operations WHERE operation IN('DO_FCALL', 'INIT_FCALL') AND operand IN ('". implode($this->blackList, "', '"). "'))\n";
+		$q .= "\nAND input.id NOT IN (SELECT input FROM operations WHERE operation IN('DO_FCALL', 'INIT_FCALL') AND operand IN ('". implode($this->blackList, "', '"). "'))\n";
 
 		$this->entries = new PhpShell_BughuntSet(PhpShell_Input, $q, $params, ['input.id' => true]);
 		foreach ($joins as $join)
