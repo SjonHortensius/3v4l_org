d99a43163c973ceb857370f86162990886dfa54b
Sjon Hortensius <sjon@hortensius.net>
2016-09-20 11:37:28 +0200

    Make UpdatePopular self-contained, update rfc documentation

--- library/PhpShell/Action/Cli/UpdatePopular.php
+++ library/PhpShell/Action/Cli/UpdatePopular.php
@@ -11,12 +11,14 @@ class PhpShell_Action_Cli_UpdatePopular extends PhpShell_Action_Cli
 
 	public function run()
 	{
+		$head = shell_exec('grep -vF compatible /var/log/nginx/access_log | tail -n 50000 |cut -d\" -f2,6|grep -v ^POST|sort -u|cut -d" " -f2|uniq -c|sort -nr 2>/dev/null|head -n10');
+
 		$active = [];
-		while (FALSE !== ($line = fgets(STDIN)))
+		foreach (explode("\n", $head) as $line)
 		{
 			list($count, $url) = explode(' ', ltrim($line));
 
-			if ($url != '/')
+			if ($url != '/' && false === strpos($url, '.json'))
 				$active[ trim($url) ] = $count;
 		}
 
