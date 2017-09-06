45ea22c303cbcdd2d6194d79c08787dfc4606e3a
Sjon Hortensius <sjon@hortensius.net>
2017-09-06 11:30:40 +0200

    Fix icons; match icon-x-y when filtering plus rewrite 2-line declarations

--- htdocs/s/update.sh
+++ htdocs/s/update.sh
@@ -9,7 +9,7 @@ cd `dirname $0`
 	head -n18 ../ext/glyphicons-halflings.css
 
 	# include only the icons we use
-	grep -rEo icon-[a-z]+ ../../tpl/ ./my.js | cut -d: -f2|sort -u|while read i; do grep -A2 --no-group-separator $i ../ext/glyphicons-halflings.css; done
+	grep -rEo icon-[a-z-]+ ../../tpl/ ./my.js | cut -d: -f2|sort -u|while read i; do grep -A2 --no-group-separator $i ../ext/glyphicons-halflings.css; done
 }| php -r "require('/srv/http/.common/Basic_Framework/library/Basic/Static.php');echo Basic_Static::cssStrip(file_get_contents('php://stdin'), 2500);" > c.css
 
 cat ./my.js | php -r "require('/srv/http/.common/jsminplus.php');ini_set('memory_limit', '256M');echo JSMinPlus::minify(file_get_contents('php://stdin'));" > c.js
