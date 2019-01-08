e58ba4386dc57844873d09684e657882cfc7233b
Sjon Hortensius <sjon@hortensius.net>
2019-01-08 12:52:43 +0100

    FunctionsUpdate - proper escaping plus output stats
    update-online - make sure mtime(tpl & library) is correct according to
    recursive check

--- library/PhpShell/Action/Cli/FunctionsUpdate.php
+++ library/PhpShell/Action/Cli/FunctionsUpdate.php
@@ -6,6 +6,8 @@ class PhpShell_Action_Cli_FunctionsUpdate extends PhpShell_Action_Cli
 
 	public function run(): void
 	{
+		$result = ['skipped'=>0, 'updated'=>0];
+
 		if (!is_dir('/tmp/php-src/Zend'))
 			throw new Exception('FEED ME A STRAY CAT @ `/tmp/php-src/`');
 		chdir('/tmp/php-src/');
@@ -13,17 +15,19 @@ class PhpShell_Action_Cli_FunctionsUpdate extends PhpShell_Action_Cli
 		Basic::$database->beginTransaction();
 		$statement = Basic::$database->prepare("INSERT INTO function (text, source) VALUES (?, ?) ON CONFLICT (text) DO UPDATE SET source=excluded.source");
 
-		$preg = self::FUNC_PREG;
-		foreach (explode("\n", `grep -nrP '$preg' --include=*.c ext/ main/ Zend/`) as $line)
+		$preg = escapeshellarg(self::FUNC_PREG);
+		foreach (explode("\n", `grep -nrP $preg --include=*.c ext/ main/ Zend/`) as $line)
 		{
 			list($file, $lineNo, $match, $trash) = explode(':', $line, 4);
 
 			if (!empty($trash) || !preg_match('~'. self::FUNC_PREG .'(.*)\)(?!;)~', $match, $m) || strlen($m[1]) > 64)
-				;#fprintf(STDERR, "mismatch, skipping: %s\n", $line);
+				$result['skipped']++;
 			else
-				$statement->execute([trim($m[1]), $file.':'.$lineNo]);
+				$result['updated'] += intval($statement->execute([trim($m[1]), $file.':'.$lineNo]));
 		}
 
 		Basic::$database->commit();
+
+		print_r($result);
 	}
 }
