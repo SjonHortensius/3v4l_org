1945c6a36e243f80f673c97499c258cc49189d85
Sjon Hortensius <sjon@hortensius.net>
2024-07-19 10:52:51 +0200

    remove JSMinPlus as it is incompatible with upcoming Promises
    used by php-wasm

--- htdocs/s/update.sh
+++ htdocs/s/update.sh
@@ -12,10 +12,6 @@ cd `dirname $0`
 	grep -rEo icon-[a-z-]+ ../../tpl/ ./my.js | cut -d: -f2|sort -u|while read i; do grep -A2 --no-group-separator $i\  ../ext/glyphicons-halflings.css; done
 }| php -r "require('/srv/http/.common/Basic_Framework/library/Basic/Static.php');echo Basic_Static::cssStrip(file_get_contents('php://stdin'), 2500);" > c.css
 
-{ cat ./my.js | php -r "require('/srv/http/.common/jsminplus.php');ini_set('memory_limit', '256M');echo JSMinPlus::minify(file_get_contents('php://stdin'));"; cat ../ext/diff.js; } > c.js
-
-grep -q object2Dom c.js || { echo Failed to execute jsminplus >&2; exit 1; }
-
 # [[ $1 == 'q' ]] && exit 0
 
 # curl -s -d compilation_level=SIMPLE_OPTIMIZATIONS -d output_format=text -d output_info=compiled_code --data-urlencode "js_code@c.js" http://closure-compiler.appspot.com/compile > c2.js && mv c2.js c.js
